30dadbbd520acbdc9bd2b8f21a22c7002604cc61
andrescueva <carlos9125@gmail.com>
2023-02-20 10:14:35 -0500

    add docstrings

--- microsvc/main.py
+++ microsvc/main.py
@@ -10,38 +10,46 @@ app = FastAPI()
 
 @app.post("/DevOps/")
 async def send_message(
+
     message: Message, x_parse_rest_api_key: str = Depends(validate_api_key)
 ):
+    """Post message protected with api key"""
     message_dict = message.dict()
     to = message_dict["to"]
     return {"message": f"Hello {to} your message will be send"}
 
 
 @app.get("/DevOps/")
-async def send_message():
+async def get_send_message():
+    """Method get not allowed"""
     raise HTTPException(status_code=405, detail="ERROR")
 
 
 @app.delete("/DevOps/")
-async def send_message():
+async def delete_send_message():
+    """Method deleye not allowed"""
     raise HTTPException(status_code=405, detail="ERROR")
 
 
 @app.put("/DevOps/")
-async def send_message():
+async def put_send_message():
+    """Method put not allowed"""
     raise HTTPException(status_code=405, detail="ERROR")
 
 
 @app.patch("/DevOps/")
-async def send_message():
+async def patch_send_message():
+    """Method patch not allowed"""
     raise HTTPException(status_code=405, detail="ERROR")
 
 
 @app.get("/jwt/")
 async def get_token(token: str = Depends(create_jwt)):
+    """endpoint to get token"""
     return {"token": token}
 
 
 @app.get("/health/")
 async def get_health_status():
+    """health status check endpoint"""
     return {"status": "OK80"}
